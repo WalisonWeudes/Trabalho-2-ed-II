5212dd4f0df3cd2824bcb0b8d91a56faf4c883b2
EmillySatiro <sousasatiroclaraemilly@gmail.com>
2024-11-20 11:14:46 -0300

    só um bagulho na estrutura

--- Arvore-binaria/ARV_BINARIA.h
+++ Arvore-binaria/ARV_BINARIA.h
@@ -3,7 +3,7 @@
 
 typedef struct ARV_BINARIA{
     char palavra_ingles[25];
-    int unidade; // unidade associada 
+    int unidade; 
     ARV_BINARIA *esquerda; 
     ARV_BINARIA *direita; 
 }ARV_BINARIA;
